89a96246d2117c0c844cc2af6237a21df5b87163
gqlx <2601179392@qq.com>
2025-02-08 17:02:33 +0800

    状态机添加

---
+++ Functions/StateMachinePattern.cs
@@ -0,0 +1,29 @@
+﻿using StudyNotes.CustomAttribute;
+using StudyNotes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyNotes.Functions
+{
+    public class StateMachinePattern
+    {
+        /// <summary>
+        /// 状态机开始
+        /// </summary>
+        [Method]
+        public void StateMachineRun()
+        {
+            StateMachine sm = new StateMachine();
+            StateA s = new StateA(sm);
+            sm.Current = s;
+            while (true)
+            {
+                sm.Execute();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Model/StateMachine_Model.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyNotes.Model
+{
+    public interface IState
+    {
+        void Execute();
+    }
+
+    public class StateA : IState
+    {
+        private StateMachine _sm;
+
+        public StateA(StateMachine sm)
+        {
+            _sm = sm;
+        }
+
+        public void Execute()
+        {
+            Debug.WriteLine("State A");
+            _sm.Current = new StateB(_sm);
+        }
+    }
+
+    public class StateB : IState
+    {
+        private StateMachine _sm;
+
+        public StateB(StateMachine sm)
+        {
+            _sm = sm;
+        }
+
+        public void Execute()
+        {
+            Debug.WriteLine("State B");
+            _sm.Current = new StateA(_sm);
+        }
+    }
+
+    public class StateMachine
+    {
+        public IState Current { get; set; }
+
+        public void Execute()
+        {
+            Current.Execute();
+        }
+    }
+}
